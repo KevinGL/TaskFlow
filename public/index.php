131c0bb550bc9a3eda40a61535d6fec37b0f04ca
kevin <kevin.progc@gmail.com>
2025-09-02 21:15:21 +0200

    Pb variables env en prod

--- public/index.php
+++ public/index.php
@@ -5,5 +5,8 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'prod';
+    $debug = (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? false);
+
+    return new Kernel($env, $debug);
 };
